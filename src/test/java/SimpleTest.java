d0f3ee7f6e1d956369912421d17e8e2c78e95334
Andrea Francia <andrea@andreafrancia.it>
2019-12-18 12:04:40 +0100

    Add a simple test and an "hello world" class

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest {
+    @Test
+    public void example() {
+        assertEquals(1,1);
+    }
+}
