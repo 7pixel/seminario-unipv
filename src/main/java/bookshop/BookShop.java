7fa282e46741582874b286f889a8ed39b1cbd94b
Andrea Francia <andrea@andreafrancia.it>
2019-12-19 12:44:03 +0100

    Save.

--- src/main/java/bookshop/BookShop.java
+++ src/main/java/bookshop/BookShop.java
@@ -1,36 +1,36 @@
 package bookshop;
 
 public class BookShop {
-    private final double DISCOUNT_EVERY_THREE_BOOKS = 20 / 100;
+    private final double DISCOUNT_EVERY_THREE_BOOKS = 20.0 / 100;
     private final Book[] books;
 
     public BookShop() {
         this.books = new Book[]{
-                new Book("Harry Potter and the Half-Blood Prince", 16.94),
-                new Book("Harry Potter and the Chamber of Secrets", 17.26),
-                new Book("Harry Potter And The Sorcerer's Stone", 12.70),
-                new Book("Harry Potter And The Goblet Of Fire", 18.28),
-                new Book("Harry Potter and the Order of the Phoenix", 18.29)
+                new Book("Harry Potter and the Half-Blood Prince", 10.00),
+                new Book("Harry Potter and the Chamber of Secrets", 10.00),
+                new Book("Harry Potter And The Sorcerer's Stone", 10.00),
+                new Book("Harry Potter And The Goblet Of Fire", 10.00),
+                new Book("Harry Potter and the Order of the Phoenix", 10.00)
         };
         this.orderTotal = 0.0;
         this.totalBooks = 0;
-        this.receipt = "";
+        this.itemsList = "";
     }
 
-    private String receipt;
+    private String itemsList;
     private double orderTotal;
     private int totalBooks;
 
     public void buy(String title) {
         double price = 0;
-        receipt += "" + title;
+        itemsList += "" + title;
         for (int i = 0; i < books.length; i++) {
             if (title.equals(this.books[i].getTitle())) {
                 price = books[i].getFullPrice();
             }
         }
         String priceStr = String.format("%2.2f", price);
-        receipt += String.format(": %s EUR\n", priceStr);
+        itemsList += String.format(": %s EUR\n", priceStr);
         orderTotal += price;
         totalBooks++;
         if (totalBooks > 3) {
@@ -43,8 +43,15 @@ public class BookShop {
     }
 
     public String getReceipt() {
-        double total = getTotal();
-        return String.format("%s\nTOTAL: %s EUR", receipt, total);
+        return String.format("%s\n%s", getItemsList(), totalLine());
+    }
+
+    public String getItemsList() {
+        return itemsList;
+    }
+
+    public String totalLine() {
+        return String.format("TOTAL: %s EUR", getTotal());
     }
 
 }
--- src/test/java/bookshop/ShopTest.java
+++ src/test/java/bookshop/ShopTest.java
@@ -1,15 +1,24 @@
 package bookshop;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class ShopTest {
-//    @Test
-//    public void something() {
-//        BookShop shop = new BookShop();
-//        shop.buy("Harry Potter and the Half-Blood Prince");
-//        shop.buy("Harry Potter And The Sorcerer's Stone");
-//        shop.buy("Harry Potter and the Order of the Phoenix");
-//
-//        System.out.println(shop.getReceipt());
-//    }
+    @Test
+    public void something() {
+        BookShop shop = new BookShop();
+
+        shop.buy("Harry Potter and the Half-Blood Prince");
+        shop.buy("Harry Potter And The Sorcerer's Stone");
+        shop.buy("Harry Potter and the Order of the Phoenix");
+        shop.buy("Harry Potter And The Goblet Of Fire");
+
+        assertEquals("Harry Potter and the Half-Blood Prince: 10.00 EUR\n" +
+                "Harry Potter And The Sorcerer's Stone: 10.00 EUR\n" +
+                "Harry Potter and the Order of the Phoenix: 10.00 EUR\n" +
+                "Harry Potter And The Goblet Of Fire: 10.00 EUR\n",
+                shop.getItemsList());
+
+    }
 }
