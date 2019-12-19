ed15d61562283435a25e81ef710f67b761a77ac5
Andrea Francia <andrea@andreafrancia.it>
2019-12-19 11:53:18 +0100

    Work in progress.

--- src/main/java/simple/Main.java
+++ src/main/java/simple/Main.java
@@ -1,3 +1,5 @@
+package simple;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ src/test/java/bookshop/ShopTest.java
@@ -0,0 +1,15 @@
+package bookshop;
+
+import org.junit.Test;
+
+public class ShopTest {
+    @Test
+    public void something() {
+        BookShop shop = new BookShop();
+        shop.buy("Harry Potter and the Half-Blood Prince");
+        shop.buy("Harry Potter And The Sorcerer's Stone");
+        shop.buy("Harry Potter and the Order of the Phoenix");
+
+        System.out.println(shop.getReceipt());
+    }
+}
