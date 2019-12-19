84fca3644adeb40847a32e1c6098ed78db74970a
Andrea Francia <andrea@andreafrancia.it>
2019-12-19 12:18:28 +0100

    Work in progress.

--- src/test/java/bookshop/ShopTest.java
+++ src/test/java/bookshop/ShopTest.java
@@ -1,17 +1,15 @@
 package bookshop;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class ShopTest {
-    @Ignore
-    @Test
-    public void something() {
-        BookShop shop = new BookShop();
-        shop.buy("Harry Potter and the Half-Blood Prince");
-        shop.buy("Harry Potter And The Sorcerer's Stone");
-        shop.buy("Harry Potter and the Order of the Phoenix");
-
-        System.out.println(shop.getReceipt());
-    }
+//    @Test
+//    public void something() {
+//        BookShop shop = new BookShop();
+//        shop.buy("Harry Potter and the Half-Blood Prince");
+//        shop.buy("Harry Potter And The Sorcerer's Stone");
+//        shop.buy("Harry Potter and the Order of the Phoenix");
+//
+//        System.out.println(shop.getReceipt());
+//    }
 }
---
+++ src/test/java/simple/DoubleTest.java
@@ -0,0 +1,39 @@
+package simple;
+
+import static org.junit.Assert.assertEquals;
+
+public class DoubleTest {
+    public static double ___ = -1;
+
+//    @Test
+//    public void comparingDoubles1() {
+//        assertEquals(1.414, Math.sqrt(2));
+//    }
+//
+//    @Test
+//    public void comparingDoubles2() {
+//        assertEquals(1.414, Math.sqrt(2), 0.01);
+//    }
+
+//    public void pitagora() {
+//        int base = 3;
+//        int height = 4;
+//        RightTriangle triangle = new RightTriangle(base, height);
+//
+//        double result = triangle.hypotenuse();
+//
+//        assertEquals(5, result);
+//    }
+
+
+//    public void pitagora2() {
+//        int base = 4;
+//        int height = 6;
+//        RightTriangle triangle = new RightTriangle(base, height);
+//
+//        double result = triangle.hypotenuse();
+//
+//        assertEquals(___, result);
+//    }
+
+}
