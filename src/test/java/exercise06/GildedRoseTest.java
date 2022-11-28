36d0120291d0e3502ecb9320841e978f1f37efbf
Brando <b.preda91@gmail.com>
2022-11-28 15:56:09 +0100

    Aggiunto esercizio 6

---
+++ src/test/java/exercise06/GildedRoseTest.java
@@ -0,0 +1,12 @@
+package exercise06;
+
+import org.junit.Test;
+
+import exercise06.GildedRose;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class GildedRoseTest {
+
+}
